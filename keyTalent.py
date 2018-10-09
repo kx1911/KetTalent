f292e1361b374a56f902f42d57681acf74ef9922
kx1911 <zhanghang106@gmail.com>
2018-10-09 11:19:29 +0800

    支持用户选择按键

--- keyTalent.py
+++ keyTalent.py
@@ -47,7 +47,14 @@ def readconfig():
 def whileloop():
     hwnds = []
     win32gui.EnumWindows(callback, hwnds)
+    keys = []
+    global comboxlist,comboxlist2,comboxlist3
+    keys.append(comboxlist.get())
+    keys.append(comboxlist2.get())
+    keys.append(comboxlist3.get())
+    keys.append(comboxlist4.get())
 
+    print(comboxlist.get())
     # hwnds = win32gui.FindWindow("notepad", None)
     # win32gui.SendMessage(hWnd,WM_CHAR,'c',0)
     global flag
@@ -66,10 +73,19 @@ def whileloop():
                 # print(hwid)
                 time.sleep(1)
                 win32gui.SetForegroundWindow(hwid)
-                keycode.mouse_rclick()
+                #keycode.mouse_rclick()
                 time.sleep(1)
+
                 for key in keys:
-                    keycode.key_input(key)
+                    print(key)
+                    if key == "鼠标左键":
+                        keycode.mouse_click()
+                        break
+                    elif key == "鼠标右键":
+                        keycode.mouse_rclick()
+                        break
+                    else:
+                        keycode.sendKey(keycode.VK_CODE[key])
                     time.sleep(0.3)
                 time.sleep(1)
         # print(hwnds)
@@ -77,7 +93,7 @@ def whileloop():
 
 def startloop():
     global flag
-    flag = not flag
+    flag = True
     thread1 = threading.Thread(target=whileloop, name='add')  # 线程对象.
     thread1.start()
 
@@ -89,35 +105,75 @@ def stoploog():
 
 def go(*args):
     # global  comboxlist
-    #print(comboxlist.get())  # 打印选中的值
-    print(comvalue)
-
-def click_2():
-    global flag2
-    flag1 = not flag2
-
+    print(comboxlist.get())  # 打印选中的值
+    #print(comvalue)
+def go2(*args):
+    # global  comboxlist
+    print(comboxlist2.get())  # 打印选中的值
+    #print(comvalue)
+def go3(*args):
+    # global  comboxlist
+    print(comboxlist3.get())  # 打印选中的值
+    #print(comvalue)
 
 def main():
     readconfig()
     # whileloop()
     Bu = Tk()
     Bu.title("KeyTalent")
-    L1 = Label(Bu, text="鼠标按键")
+    L1 = Label(Bu, text="按键1")
     L1.grid(row=0, column=0)
-    comvalue = "鼠标"
-    comboxlist = ttk.Combobox(Bu, textvariable=comvalue)  # 初始化
-    comboxlist["values"] = ("鼠标左键", "鼠标右键")
+    comvalue1 = "鼠标";comvalue2 = "鼠标";comvalue3 = "鼠标"
+    global comboxlist
+    number = StringVar()
+    comboxlist = ttk.Combobox(Bu, textvariable=number)  # 初始化
+    keys = keycode.VK_CODE.keys()
+    comboxlist["values"] = list(keys)
     comboxlist["state"] = "readonly"
     comboxlist.current(0)  # 选择第一个
     comboxlist.bind("<<ComboboxSelected>>", go)  # 绑定事件,(下拉列表框被选中时，绑定go()函数)
     comboxlist.grid(row=0, column=1)
+
+    L2 = Label(Bu, text="按键2")
+    L2.grid(row=1, column=0)
+    global comboxlist2
+    number2 = StringVar()
+    comboxlist2 = ttk.Combobox(Bu, textvariable=number2)  # 初始化
+    comboxlist2["values"] = list(keys)
+    comboxlist2["state"] = "readonly"
+    comboxlist2.current(28)  # 选择第一个
+    comboxlist2.bind("<<ComboboxSelected>>", go2)  # 绑定事件,(下拉列表框被选中时，绑定go()函数)
+    comboxlist2.grid(row=1, column=1)
+
+    L3 = Label(Bu, text="按键3")
+    L3.grid(row=2, column=0)
+    global comboxlist3
+    number3 = StringVar()
+    comboxlist3 = ttk.Combobox(Bu, textvariable=number3)  # 初始化
+    comboxlist3["values"] = list(keys)
+    comboxlist3["state"] = "readonly"
+    comboxlist3.current(28)  # 选择第一个
+    comboxlist3.bind("<<ComboboxSelected>>", go3)  # 绑定事件,(下拉列表框被选中时，绑定go()函数)
+    comboxlist3.grid(row=2, column=1)
+
+    L4 = Label(Bu, text="按键4")
+    L4.grid(row=3, column=0)
+    global comboxlist4
+    number4 = StringVar()
+    comboxlist4 = ttk.Combobox(Bu, textvariable=number4)  # 初始化
+    comboxlist4["values"] = list(keys)
+    comboxlist4["state"] = "readonly"
+    comboxlist4.current(28)  # 选择第一个
+    comboxlist4.bind("<<ComboboxSelected>>", go3)  # 绑定事件,(下拉列表框被选中时，绑定go()函数)
+    comboxlist4.grid(row=3, column=1)
+
     print(comboxlist.get)
     # E1 = Entry(Bu, bd=5)
     # E1.grid(row=0,column=2)
     Button(Bu, text=startcaption, anchor='c', fg='red', relief='ridge', compound='bottom', command=startloop)\
-        .grid(row=2, column=0)
+        .grid(row=5, column=0)
     Button(Bu, text=stopcaption, anchor='c', fg='red', relief='ridge', compound='bottom', command=stoploog)\
-        .grid(row=2, column=1)
+        .grid(row=5, column=1)
     Bu.mainloop()
 
 if __name__ == "__main__":
